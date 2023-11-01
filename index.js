7db69c354eec635b72ea937b50a598159a2b7bbe
Ayush Sharma <asrocks5@gmail.com>
2023-11-01 07:55:12 +0530

    - Changed name

--- index.js
+++ index.js
@@ -24,8 +24,8 @@ const SwitchAccessory = require('./lib/SwitchAccessory');
 const ValveAccessory = require('./lib/ValveAccessory');
 const OilDiffuserAccessory = require('./lib/OilDiffuserAccessory');
 
-const PLUGIN_NAME = 'homebridge-tuya-lan';
-const PLATFORM_NAME = 'TuyaLan';
+const PLUGIN_NAME = 'homebridge-tuya-local';
+const PLATFORM_NAME = 'tuya-local';
 
 const CLASS_DEF = {
     outlet: OutletAccessory,
